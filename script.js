af73c6a09b06221bb1c644eed2f47ce84e9ef504
KingPr0f <strange98752@gmail.com>
2023-07-05 11:55:33 +1000

    New changes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function AverageNum(numbers) {
     if (!Array.isArray(numbers) || numbers.length === 0) {
-    return 0;
+        return 0;
+    }
+    
+    const sum = numbers.reduce((acc, num) => acc + num, 0);
+    const average = sum / numbers.length;
+    return average;
 }
-const sum = numbers.reduce((acc, num) => acc + num, 0);
-const average = sum / numbers.length;
-return average;
-}
-
-const numbers = [11, 22, 33, 44, 55];
-result = AverageNum(numbers);
-console.log("Average: ", result);
+const numbers = [13, 24, 35, 45, 58];
+const result = AverageNum(numbers);
+console.log('Average is ', result);
