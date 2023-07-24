07bdb615cd0805cac1b45278bd95b39b8fb14540
KingPr0f <strange98752@gmail.com>
2023-07-24 18:53:35 +1000

    lesson

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-
-function readName() {
-    let myName='1nternal Script';
-    console.log(myName)
-}
-readName();
+( function() {
+    let sum, net = '25', tax = 5.00
+    sum = net + tax
+    console.log('sum: ' + sum + ' ' + typeof sum)
+    sum = parseFloat(net) + tax
+    console.log('sum: ' + sum + ' ' + typeof sum)
+})()
